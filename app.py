689ced5b372a619fade0a34676da529719aaaf87
mikemwai <mwai.michael@strathmore.edu>
2024-07-01 13:56:44 +0300

    project updates

--- app.py
+++ app.py
@@ -8,36 +8,35 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense, Dropout
 from tensorflow.keras.applications.vgg19 import VGG19
 
-
-base_model = VGG19(include_top=False, input_shape=(240,240,3))
+base_model = VGG19(include_top=False, input_shape=(240, 240, 3))
 x = base_model.output
-flat=Flatten()(x)
+flat = Flatten()(x)
 class_1 = Dense(4608, activation='relu')(flat)
 drop_out = Dropout(0.2)(class_1)
 class_2 = Dense(1152, activation='relu')(drop_out)
 output = Dense(2, activation='softmax')(class_2)
 model_03 = Model(base_model.inputs, output)
-model_03.load_weights('vgg_unfrozen.h5')
+model_03.load_weights('./vgg_unfrozen.h5')
 app = Flask(__name__)
 
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def get_className(classNo):
-	if classNo==0:
-		return "No Brain Tumor"
-	elif classNo==1:
-		return "Yes Brain Tumor"
+    if classNo == 0:
+        return "No Brain Tumor"
+    elif classNo == 1:
+        return "Yes Brain Tumor"
 
 
 def getResult(img):
-    image=cv2.imread(img)
+    image = cv2.imread(img)
     image = Image.fromarray(image, 'RGB')
     image = image.resize((240, 240))
-    image=np.array(image)
+    image = np.array(image)
     input_img = np.expand_dims(image, axis=0)
-    result=model_03.predict(input_img)
-    result01=np.argmax(result,axis=1)
+    result = model_03.predict(input_img)
+    result01 = np.argmax(result, axis=1)
     return result01
 
 
@@ -55,8 +54,8 @@ def upload():
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        value=getResult(file_path)
-        result=get_className(value) 
+        value = getResult(file_path)
+        result = get_className(value)
         return result
     return None
 
